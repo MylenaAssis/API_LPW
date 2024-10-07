6eae2212392a49f3e32226022d24389cfefab845
Mylena Assis <lena.assis.dev@gmail.com>
2024-10-07 15:43:50 -0300

    Rota para listar usuarios

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,24 @@
 import fastify from 'fastify';
+import { FastifyRequest, FastifyReply } from 'fastify';
 const app = fastify();
+
+const users = [
+    { id: '1', name: 'Joao', email: 'joao@unifoa.edu.br', phone: '24999000500' },
+    { id: '2', name: 'Pedro', email: 'pedro@unifoa.edu.br', phone: '24999000501'},
+    { id: '3', name: 'Maria', email: 'maria@unifoa.edu.br', phone: '24999000502'},
+    { id: '4', name: 'Rosa', email: 'rosa@unifoa.edu.br', phone: '24999000503'}
+]
+
+//rota com metodo get para listar usuários
+app.get('/users', (request: FastifyRequest, reply: FastifyReply) => {
+    return reply.status(200).send(users)
+})
 
 app.listen({
     port: 3333,
 }).then (() => {
     console.log('HTTP server runing on http://localhost:33333')
 })
+
+
+
